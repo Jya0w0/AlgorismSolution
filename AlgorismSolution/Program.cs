f263cd87cf0b95fe0a55691b56639837005e7d3f
Jya0w0 <jya0w0@gmail.com>
2023-11-15 18:30:52 +0900

    clear
    주석 정리

--- AlgorismSolution/Program.cs
+++ AlgorismSolution/Program.cs
@@ -219,9 +219,9 @@ using System.Numerics;
     {
         long[] answer = new long[n];
         {
-            for (int i = 0; i <= n; i++)
+            for (int i = 0; i < n; i++)
             {
-                answer[i] = x * (i + 1);
+                answer[i] = (long) x * (i + 1); // int로 계산하면 범위 때문에 오버플로
             }
         }
         return answer;
